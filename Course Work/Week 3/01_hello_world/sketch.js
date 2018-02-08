d3b33e57d0588232c1e51dd856f17d6991636af9
Zoie <zkoostra@gmail.com>
2018-02-07 21:24:29 -0700

    working on homework 3 for CC class. Hello, world!

---
+++ Course Work/Week 3/01_hello_world/sketch.js
@@ -0,0 +1,18 @@
+// Start the setup function
+function setup() {
+
+    // create a workspace that's 1200 pixels wide, 800 long
+    createCanvas( 1200, 800 );
+
+    // color the background turquoise
+    background( 'turquoise' );
+}
+
+
+// Declare the draw function
+function draw() {
+
+    // tell p5 to write "Hello World!"
+    // tell p5 to position Hello World at 600, 100 x,y
+    text( "Hello World!", 600, 100 );
+}
